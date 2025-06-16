27d05f48caa51eac3dfa1c721180a700288a591e
ahmednaser111 <ahmednaser7707@gmail.com>
2025-06-16 19:32:19 +0300

    Add Clerk authentication integration and onboarding pages

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OnboardingPage = () => {
+	return (
+		<main className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+			hi
+		</main>
+	);
+};
+
+export default OnboardingPage;
