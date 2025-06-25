ba480f623be6d7f197adb3d98c4590c035e7558d
ahmednaser111 <ahmednaser7707@gmail.com>
2025-06-25 03:18:15 +0300

    feat: add user model schema with validation and relationships

---
+++ lib/models/user.model.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+	id: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: String,
+		default: null,
+	},
+	bio: {
+		type: String,
+		default: null,
+		maxlength: 500,
+	},
+	threads: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Thread",
+		},
+	],
+	onboarded: {
+		type: Boolean,
+		default: false,
+	},
+	communities: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Community",
+		},
+	],
+	// password: {
+	// 	type: String,
+	// 	required: true,
+	// },
+	// createdAt: {
+	// 	type: Date,
+	// 	default: Date.now,
+	// },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
