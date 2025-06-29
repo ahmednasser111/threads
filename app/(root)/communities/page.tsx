918c2bcda9d3e4b4385e7583cf647b24fdaa7733
ahmednaser111 <ahmednaser7707@gmail.com>
2025-06-30 02:01:29 +0300

    refactor: remove unnecessary export keyword from Page component definition

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,4 +1,4 @@
-export const Page = async () => {
+const Page = async () => {
 	return (
 		<>
 			<h1 className="head-text">Communities</h1>
@@ -12,3 +12,5 @@ export const Page = async () => {
 		</>
 	);
 };
+
+export default Page;
